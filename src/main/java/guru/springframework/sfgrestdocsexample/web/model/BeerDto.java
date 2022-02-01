efe874fe22b2dee583c8969e59e74043331f7f31
Connor Wheatley <cwheatley@tier2consulting.com>
2022-02-01 09:29:43 +0000

    Fixed dependency issue with validation

--- src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -5,10 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -23,7 +21,7 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
-    @Null
+    @NotEmpty
     private UUID id;
 
     @Null
