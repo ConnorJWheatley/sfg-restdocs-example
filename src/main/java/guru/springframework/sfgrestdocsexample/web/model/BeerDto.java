3ed6b05638d46c0432dbc80b8c71e5dda38cb276
Connor Wheatley <cwheatley@tier2consulting.com>
2022-02-01 09:59:04 +0000

    Changed static imports in test class. Fixed mapstruct version

--- src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -5,7 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
